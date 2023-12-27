2bcba2f34dff792eeb8fb2b96c7214d0f589ba4d
asifshezan <asifshezan7@gmail.com>
2023-12-27 23:57:18 +0600

    Admin dashboard total count setup

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -11,12 +11,17 @@
                         <div class="float-end">
                             <i class="mdi mdi-account-multiple widget-icon"></i>
                         </div>
-                        <h5 class="text-muted fw-normal mt-0" title="Number of Customers">Customers</h5>
-                        <h3 class="mt-3 mb-3">36,254</h3>
-                        <p class="mb-0 text-muted">
-                            <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 5.27%</span>
-                            <span class="text-nowrap">Since last month</span>  
-                        </p>
+                        @php
+                            $totaluser = App\Models\user::count();
+                        @endphp
+                        <h5 class="text-muted fw-normal mt-0" title="Number of Customers">Users</h5>
+                        <h3 class="mt-3 mb-3">
+                            @if($totaluser < 10)
+                            0{{ $totaluser }}
+                            @else
+                            {{ $totaluser }}
+                            @endif
+                        </h3>
                     </div> <!-- end card-body-->
                 </div> <!-- end card-->
             </div> <!-- end col-->
@@ -27,12 +32,11 @@
                         <div class="float-end">
                             <i class="mdi mdi-cart-plus widget-icon"></i>
                         </div>
-                        <h5 class="text-muted fw-normal mt-0" title="Number of Orders">Orders</h5>
-                        <h3 class="mt-3 mb-3">5,543</h3>
-                        <p class="mb-0 text-muted">
-                            <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold"></i> 1.08%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </p>
+                        @php
+                            $totalIncome = App\Models\Income::where('income_status',1)->sum('income_amount');
+                        @endphp
+                        <h5 class="text-muted fw-normal mt-0" title="Number of Orders">Total Income</h5>
+                        <h3 class="mt-3 mb-3">{{ number_format($totalIncome,2) }}</h3>
                     </div> <!-- end card-body-->
                 </div> <!-- end card-->
             </div> <!-- end col-->
@@ -44,12 +48,11 @@
                         <div class="float-end">
                             <i class="mdi mdi-currency-usd widget-icon"></i>
                         </div>
-                        <h5 class="text-muted fw-normal mt-0" title="Average Revenue">Revenue</h5>
-                        <h3 class="mt-3 mb-3">$6,254</h3>
-                        <p class="mb-0 text-muted">
-                            <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold"></i> 7.00%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </p>
+                        @php
+                            $totalExpense = App\Models\Expense::where('expense_status',1)->sum('expense_amount');
+                        @endphp
+                        <h5 class="text-muted fw-normal mt-0" title="Average Revenue">Total Expense</h5>
+                        <h3 class="mt-3 mb-3">{{ number_format($totalExpense,2) }}</h3>
                     </div> <!-- end card-body-->
                 </div> <!-- end card-->
             </div> <!-- end col-->
@@ -60,12 +63,11 @@
                         <div class="float-end">
                             <i class="mdi mdi-pulse widget-icon"></i>
                         </div>
-                        <h5 class="text-muted fw-normal mt-0" title="Growth">Growth</h5>
-                        <h3 class="mt-3 mb-3">+ 30.56%</h3>
-                        <p class="mb-0 text-muted">
-                            <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 4.87%</span>
-                            <span class="text-nowrap">Since last month</span>
-                        </p>
+                        @php
+                            $totalSevings = $totalIncome - $totalExpense;
+                        @endphp
+                        <h5 class="text-muted fw-normal mt-0" title="Growth">Total Sevings</h5>
+                        <h3 class="mt-3 mb-3">{{ number_format($totalSevings,2)}}</h3>
                     </div> <!-- end card-body-->
                 </div> <!-- end card-->
             </div> <!-- end col-->
